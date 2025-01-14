6ad47316a97709aa57faed95058fba9f3ea4b858
Maschetti <mateusvmaschietto@gmail.com>
2025-01-14 00:07:16 -0300

    Feature: add IconButton

---
+++ components/_ui/IconButton.tsx
@@ -0,0 +1,18 @@
+import { LucideIcon } from "lucide-react";
+
+type IconProps = {
+  children?: React.ReactNode | string;
+  size?: number;
+  color?: string;
+  tooltip?: string; // Texto a ser exibido no tooltip
+};
+
+const IconButton: React.FC<IconProps> = ({ children, size = 24, color = "currentColor", tooltip }) => {
+  return (
+    <div className="flex items-center justify-center hover:cursor-pointer hover:bg-border rounded-full p-2" title={tooltip}>
+      {children}
+    </div>
+  );
+};
+
+export default IconButton;
