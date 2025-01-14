f95a71b6874e587a432110845b7e37fca7e73361
Maschetti <mateusvmaschietto@gmail.com>
2025-01-14 15:55:27 -0300

    style: add a separator color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,8 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
         text: "var(--text)",
-        border: "var(--border)"
+        border: "var(--border)",
+        separator: "var(--separator)"
       },
     },
   },
