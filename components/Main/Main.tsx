454fffc4be49abd24810bc4265d1e0d473e01d30
Maschetti <mateusvmaschietto@gmail.com>
2025-01-14 15:59:26 -0300

    style: use grid to display projects cards

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,12 +1,14 @@
 import { Aside } from "../Aside/Aside";
+import { ProjectCard } from "./ProjectCard";
 
 export const Main = () => {
 
   return (
     <main className="flex flex-row flex-1 px-10">
-      <section className="flex-1 flex flex-col relative top-10">
-        {[...Array(5)].map((_, index) => (
-          <h1 key={index}>Lastest Projects...</h1>
+      <section className="flex-1 grid grid-cols-2 gap-x-2 gap-y-2 relative p-6">
+        {[...Array(10)].map((_, index) => (
+          
+          <ProjectCard key={index} src='/images/portifolio.png' />
         ))}
       </section>
       <Aside />
