48bd2eabdc938f5ee3d7d06fd1bdcb12f13dfe6e
Maschetti <mateusvmaschietto@gmail.com>
2025-01-15 17:33:06 -0300

    feat: create a section container

---
+++ components/SemanticComponents/Sections/Section.tsx
@@ -0,0 +1,13 @@
+interface SectionProps {
+  children: React.ReactNode,
+  title: string
+}
+
+export const Section = ({children, title}: SectionProps) => {
+  return (
+    <section className="p-6 flex-1">
+      <h1 className="text-secondary text-5xl italic">{title}</h1>
+      {children}
+    </section>
+  )
+};
