855be32254609a3b6a7a8a5a283a65822eb88c99
Maschetti <mateusvmaschietto@gmail.com>
2025-01-15 17:33:49 -0300

    style: show projects as a grid

--- components/SemanticComponents/Sections/Projects.tsx
+++ components/SemanticComponents/Sections/Projects.tsx
@@ -2,11 +2,11 @@ import { ProjectCard } from "@/components/_ui/ProjectCard"
 
 export const Projects = () => {
   return (
-    <section className="flex-1 grid grid-cols-2 gap-x-2 gap-y-2 p-6">
+    <div className="grid grid-cols-2 gap-x-2 gap-y-2 w-full">
       {[...Array(2)].map((_, index) => (
         
         <ProjectCard key={index} src='/images/portifolio.png' />
       ))}
-    </section>
+    </div>
   )
 }
