114a839908d2623f16571eb12850b11ed86ec688
Maschetti <mateusvmaschietto@gmail.com>
2025-01-15 19:51:36 -0300

    feat: create a workin on component

---
+++ components/_ui/Working.tsx
@@ -0,0 +1,10 @@
+import { Wrench } from "lucide-react"
+
+export const Working = () => {
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center gap-6">
+      <Wrench size={100} className="text-secondary"/>
+      <h1 className="text-3xl">In progress...</h1>
+    </div>
+  )
+}
