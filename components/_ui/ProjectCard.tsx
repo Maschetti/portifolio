d0be989aa33b30666fb93daea5a3af5673799045
Maschetti <mateusvmaschietto@gmail.com>
2025-01-15 17:38:59 -0300

    style: make the project title colored

--- components/_ui/ProjectCard.tsx
+++ components/_ui/ProjectCard.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 export const ProjectCard = ({src}: {src: string}) => {
   return (
-    <div className="w-full max-w-4xl mx-auto p-4 hover:cursor-pointer hover:shadow-secondary hover:shadow-lg rounded-sm transition-shadow">
+    <div className="w-full max-w-4xl mx-auto p-4 hover:cursor-pointer hover:shadow-stone-400 hover:shadow-lg rounded-sm transition-shadow">
       <Image  
         src={src}
         alt="Print do aplicativo"
@@ -11,7 +11,7 @@ export const ProjectCard = ({src}: {src: string}) => {
         height={1080}
         className="rounded-xs border border-border object-cover"
       />
-      <h2 className='text-base underline underline-offset-2 uppercase font-light text-secondary'>Portifolio</h2>
+      <h2 className='text-base text-secondary underline underline-offset-2 uppercase font-light'>Portifolio</h2>
       <p className='text-sm'>This app is a way to share my experiences.</p>
       <p className='text-xs'>Tech: Nextjs, React, Typescript and Tailwindcss.</p>
     </div>
