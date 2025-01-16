6adb1cac684eb8bc019a7a65ef3aeb21b08f071b
Maschetti <mateusvmaschietto@gmail.com>
2025-01-16 18:54:39 -0300

    reset repository to 0

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,15 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        text: "var(--text)",
-        border: "var(--border)",
-        separator: "var(--separator)",
-        secondary: "var(--secondary)"
       },
     },
   },
