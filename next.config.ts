b27e6381c8dc16f42da266ca945a69bd0c8b728a
Maschetti <mateusvmaschietto@gmail.com>
2025-01-15 22:26:58 -0300

    fix: enoent error

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,18 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  distDir: 'build',
+  i18n: {
+    locales: ['en', 'pt'],
+    defaultLocale: 'en',
+  },
+  exportPathMap: async function () {
+    return {
+      '/en': { page: '/[locale]', query: { locale: 'en' } },
+      '/pt': { page: '/[locale]', query: { locale: 'pt' } },
+    };
+  },
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
