cb05d7ef3d982c0bed99f86e1a1476bbe521da36
Maschetti <mateusvmaschietto@gmail.com>
2025-01-16 00:01:15 -0300

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import { useTheme } from '@/hooks/useTheme';
-import { routing } from '@/i18n/routing';
+import { useTheme } from '../hooks/useTheme';
+import { routing } from '../i18n/routing';
+import { useRouter } from 'next/router'
 import { Metadata } from 'next';
-import { notFound } from 'next/navigation';
+import { notFound, useRouter as useNavRouter } from 'next/navigation';
 import React from 'react';
 export async function generateStaticParams() {
   const locales = ['en', 'pt']; // Liste aqui todos os locais suportados
@@ -16,7 +17,13 @@ export const metadata: Metadata = {
 type Locale = 'en' | 'pt';
 type Params = {locale: Locale};
 export default async function Layout({ children, params }: { children: React.ReactNode, params: Promise<Params>; }) {
+  const nextRouter = useRouter();
+  if (!nextRouter.isFallback) {
+    notFound();
+  }
   const {locale} = (await params);
+
+  const router = useNavRouter();
   
   if (!routing.locales.includes(locale as any)) {
     notFound();
